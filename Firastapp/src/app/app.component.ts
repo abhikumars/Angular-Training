b60127dc082dea9ab745c211bea9cb944b2ea964
Santosh Kumar Senapati <Santosh.Senapati@Pratian.com>
2019-10-18 18:55:38 +0530

    Todays Checkin

--- Firastapp/src/app/app.component.ts
+++ Firastapp/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef, ChangeDetectionStrategy, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  changeDetection:ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
   title = 'Firastapp';
+  counter = 0;
+  constructor(private ref: ChangeDetectorRef) {
+    setInterval(() => {
+      this.counter++;
+      // require view to be updated
+
+      this.ref.detectChanges();
+    }, 1000);
+  }
+
+
+  tagpClicked(data){
+    console.log("Parent");
+  }
+  tagclicked(data){
+    console.log("child");
+  }
 }
